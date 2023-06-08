8bd0a856303b2ab2f4c8c067c0d68ffcd7685e3a
kamitechie <kamitechie@gmail.com>
2023-06-08 10:01:32 +0300

    added error checking to exercise 5

--- tests_parameterized_main.py
+++ tests_parameterized_main.py
@@ -8,10 +8,16 @@ class TestMain(unittest.TestCase):
     @parameterized.expand([
         ('test_exercise_reverse_fullname_basic', 'Anna', 'Jo', 'oJ annA'),
         ('test_exercise_reverse_fullname_uppercase', 'WILL', 'SHO', 'OHS LLIW'),
-        ('test_exercise_reverse_fullname_lowercase', 'foo', 'ally', 'ylla oof')
+        ('test_exercise_reverse_fullname_lowercase', 'foo', 'ally', 'ylla oof'),
+        ('test_exercise_reverse_fullname_not_string', 123, 123, None, TypeError),
+        ('test_exercise_reverse_fullname_none', None, 'smth', None, TypeError)
     ])
-    def test_exercise_reverse_fullname(self, name, in_1, in_2, expected, raises=None):
-        assert main.exercise_reverse_name(in_1, in_2) == expected
+    def test_exercise_reverse_fullname(self, name, in_1, in_2, expected, expected_error=None):
+        if expected_error is None:
+            assert main.exercise_reverse_name(in_1, in_2) == expected
+        else:
+            with self.assertRaises(TypeError):
+                main.exercise_reverse_name(in_1, in_2)
 
 
 if __name__ == '__main__':
